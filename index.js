e9eaddbb2a113ba3cda12c09b20d83013f670d97
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-12 12:31:48 -0700

    fizzbuzz working

--- index.js
+++ index.js
@@ -23,7 +23,21 @@ function oddishOrEvenish(number) {
 }
 
 function at(arr, index) {
-    return index > -1 ? arr[index] : arr[arr.length + index];
+  return index > -1 ? arr[index] : arr[arr.length + index];
 }
 
-module.exports = { reverseWords, titleCase, oddishOrEvenish, at };
+function fizzBuzz(number) {
+  const arr = [];
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else arr.push(i);
+  }
+  return arr;
+}
+
+module.exports = { reverseWords, titleCase, oddishOrEvenish, at, fizzBuzz };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { reverseWords, titleCase, oddishOrEvenish, at } = require(".");
+const { reverseWords, titleCase, oddishOrEvenish, at, fizzBuzz } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
@@ -12,7 +12,27 @@ describe("reverseWords", () => {
     expect(oddishOrEvenish(44)).toBe("Evenish");
   });
   it("should return the item at the index provided", () => {
-    expect(at(['a', 'b', 'c', 'd', 'e'], 1)).toBe('b');
-    expect(at(['a', 'b', 'c', 'd', 'e'], -2)).toBe('d');
+    expect(at(["a", "b", "c", "d", "e"], 1)).toBe("b");
+    expect(at(["a", "b", "c", "d", "e"], -2)).toBe("d");
+  });
+  it("should return an array that contains all numbers in order from 1 to n, with multiples of 3 showing up as Fizz, multiples of 5 showing up as Buzz, and multiples of 3 and 5 showing up as FizzBuzz", () => {
+    expect(fizzBuzz(6)).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz"]);
+    expect(fizzBuzz(15)).toEqual([
+      1,
+      2,
+      "Fizz",
+      4,
+      "Buzz",
+      "Fizz",
+      7,
+      8,
+      "Fizz",
+      "Buzz",
+      11,
+      "Fizz",
+      13,
+      14,
+      "FizzBuzz",
+    ]);
   });
 });
