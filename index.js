cd8e01ffd9bbe80cbdee727d2d2e4e09246209be
Anddy <alonzoa2015@gmail.com>
2022-07-12 10:43:48 -0700

    upper case the first letter of a word working

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ function reverseWords(sentence) {
     .join(" ");
 }
 
-module.exports = { reverseWords };
+function titleCase(sentence) {
+    const words = sentence.split(' '); //splits sentence into array
+    const final = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)); //splits each word into array and capitalizes first letter
+    return final.join(' ')
+}
+module.exports = { reverseWords, titleCase };
--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
-const { reverseWords } = require(".");
+const { reverseWords, titleCase } = require(".");
 
 describe("reverseWords", () => {
   it("should reverse the words of a sentence", () => {
     expect(reverseWords("alchemy rocks gold")).toBe("ymehcla skcor dlog");
+  });
+  it("should capitlaze the first letter of the words of a sentence", () => {
+    expect(titleCase("alchemy rocks gold")).toBe("Alchemy Rocks Gold");
   });
 });
